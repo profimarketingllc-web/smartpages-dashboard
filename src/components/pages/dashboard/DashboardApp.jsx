ad313523e8be137aae54388e4a6ff2dd2434ec1c
Profi Marketing <profi.marketing.llc@gmail.com>
2026-01-29 14:34:35 +0100

    Update DashboardApp.jsx

--- src/components/pages/dashboard/DashboardApp.jsx
+++ src/components/pages/dashboard/DashboardApp.jsx
@@ -1,45 +1,13 @@
-import { createSignal } from "solid-js";
-
-/* Cards */
 import CustomerCard from "./CustomerCard.jsx";
 import ImprintCard from "./ImprintCard.jsx";
 import PrivacyCard from "./PrivacyCard.jsx";
 
-/* Modals */
-import CustomerModal from "./modals/CustomerModal.jsx";
-import ImprintModal from "./modals/ImprintModal.jsx";
-import PrivacyModal from "./modals/PrivacyModal.jsx";
-
 export default function DashboardApp() {
-  // 🔐 Modal States (zentral!)
-  const [showCustomer, setShowCustomer] = createSignal(false);
-  const [showImprint, setShowImprint] = createSignal(false);
-  const [showPrivacy, setShowPrivacy] = createSignal(false);
-
   return (
-    <>
-      {/* ================= CARDS ================= */}
-      <div class="space-y-6">
-        <CustomerCard onEdit={() => setShowCustomer(true)} />
-        <ImprintCard onEdit={() => setShowImprint(true)} />
-        <PrivacyCard onEdit={() => setShowPrivacy(true)} />
-      </div>
-
-      {/* ================= MODALS ================= */}
-      <CustomerModal
-        open={showCustomer()}
-        onClose={() => setShowCustomer(false)}
-      />
-
-      <ImprintModal
-        open={showImprint()}
-        onClose={() => setShowImprint(false)}
-      />
-
-      <PrivacyModal
-        open={showPrivacy()}
-        onClose={() => setShowPrivacy(false)}
-      />
-    </>
+    <div class="space-y-6">
+      <CustomerCard />
+      <ImprintCard />
+      <PrivacyCard />
+    </div>
   );
 }
