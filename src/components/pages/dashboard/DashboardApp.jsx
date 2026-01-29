43b12e017b2488af90a2941e9de2b03ccc0d9d9c
Profi Marketing <profi.marketing.llc@gmail.com>
2026-01-29 14:42:39 +0100

    Update DashboardApp.jsx

--- src/components/pages/dashboard/DashboardApp.jsx
+++ src/components/pages/dashboard/DashboardApp.jsx
@@ -2,12 +2,24 @@ import CustomerCard from "./CustomerCard.jsx";
 import ImprintCard from "./ImprintCard.jsx";
 import PrivacyCard from "./PrivacyCard.jsx";
 
+/* Modal */
+import CustomerModal from "./modals/CustomerModal.jsx";
+
 export default function DashboardApp() {
+  const [showCustomer, setShowCustomer] = createSignal(false);
+
   return (
-    <div class="space-y-6">
-      <CustomerCard />
-      <ImprintCard />
-      <PrivacyCard />
-    </div>
+    <>
+      {/* CARD */}
+      <div class="space-y-6">
+        <CustomerCard onEdit={() => setShowCustomer(true)} />
+      </div>
+
+      {/* MODAL */}
+      <CustomerModal
+        open={showCustomer()}
+        onClose={() => setShowCustomer(false)}
+      />
+    </>
   );
 }
