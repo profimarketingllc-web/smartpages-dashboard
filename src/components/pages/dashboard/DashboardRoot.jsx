f6b8ddad8a568bcbb267f2584a523598ea5868c0
Profi Marketing <profi.marketing.llc@gmail.com>
2026-01-30 13:00:48 +0100

    Update DashboardRoot.jsx

--- src/components/pages/dashboard/DashboardRoot.jsx
+++ src/components/pages/dashboard/DashboardRoot.jsx
@@ -1,10 +1,8 @@
-/* Cards */
 import SystemMessage from "./SystemMessage.jsx";
 import CustomerCard from "./CustomerCard.jsx";
 import ImprintCard from "./ImprintCard.jsx";
 import PrivacyCard from "./PrivacyCard.jsx";
 
-/* Modals */
 import EditCustomerModal from "./modals/EditCustomerModal.jsx";
 import EditImprintModal from "./modals/EditImprintModal.jsx";
 import EditPrivacyModal from "./modals/EditPrivacyModal.jsx";
@@ -12,13 +10,28 @@ import EditPrivacyModal from "./modals/EditPrivacyModal.jsx";
 export default function DashboardRoot() {
   return (
     <>
-      {/* ---------------- System Message ---------------- */}
-      <SystemMessage />
+      {/* ---------------- Dashboard Grid ---------------- */}
+      <div class="grid grid-cols-1 gap-6 lg:grid-cols-12">
+        {/* CustomerCard – breit */}
+        <div class="lg:col-span-8">
+          <CustomerCard />
+        </div>
 
-      {/* ---------------- Cards ---------------- */}
-      <CustomerCard />
-      <ImprintCard />
-      <PrivacyCard />
+        {/* SystemMessage – schmal rechts */}
+        <div class="lg:col-span-4">
+          <SystemMessage />
+        </div>
+
+        {/* ImprintCard – volle Breite */}
+        <div class="lg:col-span-12">
+          <ImprintCard />
+        </div>
+
+        {/* PrivacyCard – volle Breite */}
+        <div class="lg:col-span-12">
+          <PrivacyCard />
+        </div>
+      </div>
 
       {/* ---------------- Modals ---------------- */}
       <EditCustomerModal />
